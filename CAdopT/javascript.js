ccc288cdb9020abbfbc5faaf3d259e2d3cb9b632
TsaridouNikoleta <152155941+TsaridouNikoleta@users.noreply.github.com>
2025-12-17 02:06:56 +0200

    Update javascript.js
    Removing the unnecessary names for the objects in the "cats" array.
    in line 61:  adding the class "catListImg" for the img.
    in line 63: adding in every list id's the cat's id.
    in line 64: adding in every list class' the class "clickableList".
    Creating the modal.

--- CAdopT/javascript.js
+++ CAdopT/javascript.js
@@ -1,6 +1,6 @@
 // cats for adoption
 const cats =[
-    cat1={
+    {
         id: 1,
         name: "Kevin",
         gender: "Male",
@@ -8,7 +8,7 @@ const cats =[
         age: "7 months",
         bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quam cupiditate fuga assumenda dolorem natus quos libero eveniet asperiores est"
     },
-    cat2={ 
+    { 
         id: 2,
         name: "Kiara",
         gender: "Female",
@@ -16,7 +16,7 @@ const cats =[
         age: "4 months",
         bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quam cupiditate fuga assumenda dolorem natus quos libero eveniet asperiores est"        
     },
-    cat3={ 
+    { 
         id: 3,
         name: "Michael",
         gender: "Male",
@@ -24,7 +24,7 @@ const cats =[
         age: "3",
         bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quam cupiditate fuga assumenda dolorem natus quos libero eveniet asperiores est"
     },
-    cat4={ 
+    { 
         id: 4,
         name: "Roger",
         gender: "Male",
@@ -32,7 +32,7 @@ const cats =[
         age: "2",
         bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quam cupiditate fuga assumenda dolorem natus quos libero eveniet asperiores est"
     },
-    cat5={
+    {
         id: 5,
         name: "Zara",
         gender: "Female",
@@ -48,18 +48,96 @@ if (cats.length > 0){
     status.innerText = "CATS that are available:";
     status.style.fontSize = "50px";
     status.style.color = "#28ad85";
-    status.style.border = "2px solid black"
+    status.style.border = "2px solid black";
 }
 
 // placing cats on adopt.html list
 cats.forEach(cat => {
-    const ul = document.querySelector('ul')
+    const ul = document.querySelector('ul');
     const listofcat = document.createElement("li");
     const imgofcat = document.createElement("img");
     const h2name = document.createElement("h2");
     imgofcat.src = cat.pic;
+    imgofcat.classList.add("catListImg");
     h2name.innerText = cat.name;
+    listofcat.id = cat.id;
+    listofcat.classList.add("clickableList");
     ul.appendChild(listofcat);
     listofcat.appendChild(imgofcat);
-    listofcat.appendChild(h2name)
+    listofcat.appendChild(h2name);
 });
+
+// Modal for cats Bio
+
+const modalBioCatt = document.getElementsByClassName("modalBioCat")[0];
+
+
+function createModalBiocat(cat, a){
+
+    //creating elements
+    const img = document.createElement("img");
+    const pName = document.createElement("p");
+    const pAge = document.createElement("p");
+    const pGender = document.createElement("p");
+    const pBio = document.createElement("p");
+
+    //setting the elements with the cat's info
+    img.src = cat[a].pic;
+    img.classList.add("imgCatModal");
+    pName.innerText = cat[a].name;
+    pAge.innerText = cat[a].age+" years old";
+    pGender.innerText = cat[a].gender;
+    pBio.innerText = cat[a].bio;
+
+    //getting the the id's from the table
+    const tblimg = document.getElementById("tblImg");
+    const tblName = document.getElementById("tblName");
+    const tblAge = document.getElementById("tblAge");
+    const tblGender = document.getElementById("tblGender");
+    const tblBio = document.getElementById("tblBio");
+
+    //adding the cat's info in the table
+    tblimg.appendChild(img);
+    tblName.appendChild(pName);
+    tblAge.appendChild(pAge);
+    tblGender.appendChild(pGender);
+    tblBio.appendChild(pBio);
+
+
+}
+
+//opening modal
+const clickableList = document.getElementsByClassName("clickableList");
+
+for (let i = 0 ; i < clickableList.length; i++) {
+   clickableList[i].addEventListener("click", () => createModalBiocat(cats, i)); 
+   clickableList[i].onclick = function() {
+        modalBioCatt.style.display = "block";
+}
+}
+
+
+function deletingCatBio(){
+  
+    //getting the the id's from the table
+    const tblimg = document.getElementById("tblImg");
+    const tblName = document.getElementById("tblName");
+    const tblAge = document.getElementById("tblAge");
+    const tblGender = document.getElementById("tblGender");
+    const tblBio = document.getElementById("tblBio");
+
+    //deleting the previous cat's info 
+    tblimg.removeChild(tblimg.firstElementChild);
+    tblName.removeChild(tblName.firstElementChild);
+    tblAge.removeChild(tblAge.firstElementChild);
+    tblGender.removeChild(tblGender.firstElementChild);
+    tblBio.removeChild(tblBio.firstElementChild);
+}
+
+//closing button for modal
+const spanClose = document.getElementsByClassName("close")[0];
+
+spanClose.onclick = function() {
+  modalBioCatt.style.display = "none";
+  deletingCatBio();
+}
